088490e7678cfecb5725082e927d2ac5ff1f4910
Pavel <pavel.lyamin2005@mail.ru>
2024-11-17 14:07:56 +0700

    Тесты и функции заглушки

---
+++ Mini_Task_For_Lection6/Mini_Task_For_Lection6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Mini_Task_For_Lection6
+{
+    public class MyTask 
+    {
+        public string[] GetText()
+        {
+            throw new Exception("Error");
+        }
+
+        public bool CheckingPossibilityOfFormatting(string text)
+        {
+            throw new Exception("Error_1");
+        }
+
+        public int RequiredNumberOfSpaces(string text, int k)
+        {
+            throw new Exception("Error_2");
+        }
+
+        public string FormattingText()
+        {
+            throw new Exception("Error_3");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Mini_Task_For_Lection6/TestsForMyTask/UnitTest1.cs
@@ -0,0 +1,31 @@
+using Mini_Task_For_Lection6;
+
+namespace TestsForMyTask
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Ruturn_Required_Number_Of_Spaces()
+        {
+            MyTask myTask = new MyTask();
+            int check = myTask.RequiredNumberOfSpaces("  Привет!  ", 20);
+            Assert.Equal(9,check);
+        }
+
+        [Fact]
+        public void CheckLenght_WhenLenghtTextLessWidth_ReturnTrue()
+        {
+            MyTask myTask = new MyTask();
+            bool check = myTask.CheckingPossibilityOfFormatting("   RandomString   ");
+            Assert.True(check);
+        }
+
+        [Fact]
+        public void Return_Right_Format()
+        {
+            MyTask myTask = new MyTask();
+            string check = myTask.FormattingText();
+            Assert.Equal("      Привет!       ", check);
+        }
+    }
+}
